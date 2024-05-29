55b5ee49421a0744cd7c1519f6c34798058ebeee
Sar2580P <sporwal1818@gmail.com>
2024-05-29 10:37:57 +0530

    completed detection on dummy dataset

--- datasets/download.py
+++ datasets/download.py
@@ -1,10 +1,12 @@
-import fiftyone.zoo 
+import fiftyone.zoo
 
 dataset = fiftyone.zoo.load_zoo_dataset(
               "open-images-v6",
               splits="validation",
               label_types=["segmentations"],
-              classes=["Dog", "Horse", "Woman"],
-              max_samples=300,
-              dataset_dir = 'datasets/seg',
+              classes=[
+                    "Dog", "Horse", "Woman", "Accordion", "Adhesive tape", "Aircraft", "Alarm clock",
+                    "Alpaca", "Ambulance", "Ant", "Antelope", "Apple"],
+              max_samples=3000,
+              dataset_dir = 'datasets/segmentation/open-images-v6',
           )
