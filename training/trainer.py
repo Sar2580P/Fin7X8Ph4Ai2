e57e34ce288f8c3edfb48c47ea649a5fe5c1e832
Sar2580P <sporwal1818@gmail.com>
2024-07-17 17:38:41 +0530

    written code for logging images

--- training/trainer.py
+++ training/trainer.py
@@ -6,6 +6,7 @@ from pytorch_lightning.loggers import WandbLogger, CSVLogger
 import os
 from training.callbacks import early_stop_callback, checkpoint_callback, rich_progress_bar, rich_model_summary
 from processing.utils import read_yaml_file
+import wandb 
 
 data_module = SegmentationDataModule(loader_config_path='configs/trainer.yaml')
 model = UNet_Variants(config_path='configs/unet_family.yaml')
@@ -23,9 +24,33 @@ run_name = f"lr-{training_config['lr']}__bs-{training_config['BATCH_SIZE']}__dec
 wandb_logger = WandbLogger(project= f"{model.name}", name = training_config['ckpt_file_name'])
 csv_logger = CSVLogger(training_config['dir']+f"/{model.name}/"+'/logs/'+  training_config['ckpt_file_name'])
 
+def log_images(logger:WandbLogger):
+    train_cols = ["caption", "sentinal-2_image", "ground_mask", "predicted_mask"]
+    test_cols = ["caption", "sentinal-2_image", "predicted_mask"]
+    train_data, test_data = [] , []
+    
+    for i in range(50):
+        
+        train_data.append([f"train_{i}.tif", wandb.Image(data_or_path = f"{training_config['img_dir']}/train_{i}.tif"), 
+                           wandb.Image(data_or_path = f"{training_config['mask_dir']}/train_mask{i}.npy"), 
+                           wandb.Image(data_or_path = f"results/output_masks/train_mask{i}.npy")])
+        
+        test_data.append([f"test_{i}.tif", wandb.Image(data_or_path = f"{training_config['img_dir']}/test_{i}.tif"), 
+                          wandb.Image(data_or_path = f"results/output_masks/test_mask{i}.npy")])
+
+    logger.log_table(key="Training Samples", columns=train_cols, data=train_data)
+    logger.log_table(key="Test Samples", columns=test_cols, data=test_data)
+
 #_____________________________________________________________________________________________________________
 trainer = Trainer(callbacks=[early_stop_callback, checkpoint_callback, rich_progress_bar, rich_model_summary], 
                   accelerator = 'cpu' ,max_epochs=training_config['MAX_EPOCHS'], logger=[wandb_logger, csv_logger])  
  
 trainer.fit(model=segmentation_setup, datamodule=data_module)
 trainer.test(datamodule=data_module)
+
+#_____________________________________________________________________________________________________________
+# logging hyper-parameters
+wandb_logger.log_hyperparams(training_config)
+
+# logging images
+log_images(wandb_logger)
