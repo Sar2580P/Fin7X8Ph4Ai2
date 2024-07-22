7794a4ba196672eba91ad08b3bf82b7329d39678
Shailesh Kumar <98047596+captain-peroxide@users.noreply.github.com>
2024-07-22 21:41:24 +0530

    normalize

--- processing/data_loading.py
+++ processing/data_loading.py
@@ -35,7 +35,8 @@ class SegmentationDataset(Dataset):
             transforms.RandomRotation(degrees=10),
             transforms.RandomHorizontalFlip(p=0.6),
             AutoAugment(policy=AutoAugmentPolicy.IMAGENET),
-            transforms.ToTensor()
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
     # @property
